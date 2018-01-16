e1ec5c11b1783c21632934847fe572216354fdea
neilw <neilw@synnex.org>
2018-01-16 18:39:12 +0800

    test for jenkins auto-deplyment 2

--- web/src/main/java/com/tipcrm/web/publicapi/TestApi.java
+++ web/src/main/java/com/tipcrm/web/publicapi/TestApi.java
@@ -7,6 +7,7 @@ import com.tipcrm.web.util.ResponseHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,8 +22,8 @@ public class TestApi {
     @Autowired
     private TestService testService;
 
-    @RequestMapping(value = "test", method = RequestMethod.GET)
-    public JsonEntity<TestEntity> test(@RequestParam("id") Long id) {
+    @RequestMapping(value = "query/{userId}", method = RequestMethod.GET)
+    public JsonEntity<TestEntity> queryByUserId(@PathVariable("userId") Long id) {
         logger.info("id = " + id);
         return ResponseHelper.createInstance(testService.findById(id));
     }
