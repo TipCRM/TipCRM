f1f9cd0c905f0c37cafdf38443add06a737e72a3
brothelul <1285823170@qq.com>
2018-02-01 23:11:41 +0800

    Dev (#16)
    * Create gui
    
    * Delete gui
    
    * Create README.md
    
    * version 1.0.0 add the init pages, contain login/register/error/demo page
    
    * Update README.md
    
    * Update README.md

---
+++ web/gui/web/src/routes/Demo/Test.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2018/2/1.
+ */
+import React from 'react';
+
+const Demo = () =>{
+  return(
+    <div style={{textAlign:'center',marginTop:'100px'}}>
+      <h1>Demo page</h1>
+    </div>
+  );
+}
+
+export default Demo;
