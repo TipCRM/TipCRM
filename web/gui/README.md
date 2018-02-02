2a7b1b0fd3b3d48a1827ef26f494a73875a8f345
mosesc <mosesc@synnex.com>
2018-02-02 18:46:20 +0800

    remove the wrong location of gui and init side menus

--- web/gui/README.md
+++
@@ -1,2 +0,0 @@
-##Introduce
-Tip CRM will be visited by different devices. You can visit it in you PC web viewer.
--- web/src/main/gui/web/src/routes/Index/Index.js
+++ web/src/main/gui/web/src/routes/Index/Index.js
@@ -1,14 +1,14 @@
 /**
- * Created by Administrator on 2018/2/1.
+ * Created by mosesc on 02/02/18.
  */
 import React from 'react';
 
-const Demo = () =>{
+const Index = () =>{
   return(
     <div style={{textAlign:'center',marginTop:'100px'}}>
-      <h1>Demo page</h1>
+      <h1>Index page</h1>
     </div>
   );
 }
 
-export default Demo;
+export default Index;
