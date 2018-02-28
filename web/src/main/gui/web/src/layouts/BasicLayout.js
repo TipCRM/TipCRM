d3fbac78273e1c46d615c34752b41c9aa7fd711b
mosesc <mosesc@synnex.com>
2018-02-28 19:14:18 +0800

    change Customer page, v2.02，get the value from api

--- web/src/main/gui/web/src/layouts/BasicLayout.js
+++ web/src/main/gui/web/src/layouts/BasicLayout.js
@@ -113,10 +113,14 @@ class BasicLayout extends React.PureComponent {
     // According to the url parameter to redirect
     // 这里是重定向的,重定向到 url 的 redirect 参数所示地址
     const urlParams = new URL(window.location.href);
-    const redirect = urlParams.searchParams.get('redirect') || '/index';
+    const redirect = urlParams.searchParams.get('redirect');
     // Remove the parameters in the url
-    urlParams.searchParams.delete('redirect');
-    window.history.pushState(null, 'redirect', urlParams.href);
+    if (redirect) {
+      urlParams.searchParams.delete('redirect');
+      window.history.replaceState(null, 'redirect', urlParams.href);
+    } else {
+      return '/index';
+    }
     return redirect;
   }
   handleMenuCollapse = (collapsed) => {
