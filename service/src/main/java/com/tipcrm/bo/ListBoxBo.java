0da30493cfdbcf5d3e3ec830f26809d1aca90a6a
wanjinzhong <1051750377@qq.com>
2018-03-01 23:53:01 +0800

    add listBoxApi

---
+++ service/src/main/java/com/tipcrm/bo/ListBoxBo.java
@@ -0,0 +1,29 @@
+package com.tipcrm.bo;
+public class ListBoxBo {
+    private Integer id;
+    private String displayName;
+
+    public ListBoxBo(Integer id, String displayName) {
+        this.id = id;
+        this.displayName = displayName;
+    }
+
+    public ListBoxBo() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+}
