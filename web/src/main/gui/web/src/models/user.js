7f31dafc0830bfce6d96b2d566e2075dace7524a
mosesc <mosesc@synnex.com>
2018-04-08 11:19:56 +0800

    add user api

--- web/src/main/gui/web/src/models/user.js
+++ web/src/main/gui/web/src/models/user.js
@@ -1,4 +1,7 @@
-import { userLogin, fetchCurrentUser, fetchUserByName} from '../services/api';
+import { userLogin, fetchCurrentUser,
+  fetchUserByName, createNewUser,
+  fetchUserDetailInfo, disMissUser,
+  updateUserInfo} from '../services/api';
 import {message} from 'antd';
 import {routerRedux} from 'dva/router';
 
@@ -6,6 +9,7 @@ export default {
   namespace: 'user',
   state: {
     currentUser: {},
+    selectUser: {},
     users:[],
   },
   effects: {
@@ -34,7 +38,34 @@ export default {
         type: 'saveUsers',
         payload: response.data,
       });
+    },
+    *createNewUser({payload}, {call, put}){
+      const response = yield call(createNewUser, payload);
+      if (response.status === 200){
+        message.success('创建员工成功');
+        yield put()
+      }
+    },
+    *getUserDetailInfo({payload}, {call, put}){
+      const response = yield call(fetchUserDetailInfo, payload);
+      yield put({
+        type: 'saveSelectUser',
+        payload: response.data,
+      });
+    },
+    *disMissUser({payload}, {call, put}){
+      const response = yield call(disMissUser, payload);
+      if (response.status === 200){
+        message.success('离职员工成功');
+      }
+    },
+    *updateUser({payload}, {call, put}){
+      const response = yield call(updateUserInfo, payload);
+      if (response.status === 200){
+        message.success('修改员工信息成功');
+      }
     }
+
   },
 
   reducers: {
@@ -56,6 +87,12 @@ export default {
       return{
         ...state,
         users: payload,
+      }
+    },
+    saveSelectUser(state, {payload}){
+      return{
+        ...state,
+        selectUser: payload,
       }
     }
   },
