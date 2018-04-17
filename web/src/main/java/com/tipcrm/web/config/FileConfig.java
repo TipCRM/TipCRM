003347308547cf163f1dd44a7fe23fb3fa39bf95
wanjinzhong <1051750377@qq.com>
2018-04-17 21:56:42 +0800

    debug file upload

--- web/src/main/java/com/tipcrm/web/config/FileConfig.java
+++ web/src/main/java/com/tipcrm/web/config/FileConfig.java
@@ -1,6 +1,8 @@
 package com.tipcrm.web.config;
 import javax.annotation.PostConstruct;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,8 +11,11 @@ public class FileConfig {
     @Value("${uploadfile.base-dir}")
     private String baseUrl;
 
+    private Logger logger = LoggerFactory.getLogger(FileConfig.class);
+
     @PostConstruct
     public void init() {
+        logger.info("Read base url from application config =======> " + baseUrl);
         com.tipcrm.constant.FileConfig.baseUrl = baseUrl;
     }
 }
