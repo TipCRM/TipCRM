9e89f5c4c194e2e78e0db5fe348ecc3a9e02f128
wanjinzhong <1051750377@qq.com>
2018-04-20 23:31:55 +0800

    add get user detail api

--- web/src/main/java/com/tipcrm/web/publicapi/UserApi.java
+++ web/src/main/java/com/tipcrm/web/publicapi/UserApi.java
@@ -7,13 +7,16 @@ import com.tipcrm.bo.QueryResultBo;
 import com.tipcrm.bo.UserBasicBo;
 import com.tipcrm.bo.UserBo;
 import com.tipcrm.bo.UserExtBo;
+import com.tipcrm.constant.Constants;
 import com.tipcrm.service.UserService;
 import com.tipcrm.service.WebContext;
 import com.tipcrm.web.util.JsonEntity;
 import com.tipcrm.web.util.ResponseHelper;
 import io.swagger.annotations.Api;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,7 +36,7 @@ public class UserApi {
     private UserService userService;
 
     @RequestMapping(value = "user/me", method = RequestMethod.GET)
-    public JsonEntity<UserExtBo> getUser() {
+    public JsonEntity<UserExtBo> getMe() {
         Integer userId = webContext.getCurrentUserId();
         return ResponseHelper.createInstance(userService.getUserByUserId(userId));
     }
@@ -46,5 +49,11 @@ public class UserApi {
     @RequestMapping(value = "user/query", method = RequestMethod.POST)
     public JsonEntity<QueryResultBo<UserBo>> queryUser(@RequestBody QueryRequestBo queryRequestBo) {
         return ResponseHelper.createInstance(userService.queryUser(queryRequestBo));
+    }
+
+    @RequestMapping(value = "user/{userId}", method = RequestMethod.GET)
+    @RequiresPermissions(value = {Constants.Permission.USER_DETAIL_VIEW})
+    public JsonEntity<UserExtBo> getUser(@PathVariable(name = "userId") Integer userId) {
+        return ResponseHelper.createInstance(userService.getUserByUserId(userId));
     }
 }
